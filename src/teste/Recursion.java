ef8dfd20137b1fde3a9a61cce68c076f2c098d73
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-08-16 13:06:41 -0300

    Add fibonacci interactive

--- src/teste/Recursion.java
+++ src/teste/Recursion.java
@@ -1,31 +1,59 @@
-package teste;
+package collegetasks;
 
-public class Recursion {
+public class CollegeTasks {
 
     public static void main(String[] args) {
-        soma(500);
-        
-        regressiva(15);   
+        //soma(500);
+        //regressiva(15);
+        fibonacciInt(34);
     }
 
-    
     public static int soma(int n) {
         if (n == 1) {
             return 1;
-        }
-        else{
+        } else {
             System.out.println(n);
-            return (n + soma(n -1));
+            return (n + soma(n - 1));
         }
     }
-    
-    public static int regressiva(int n){
-        if(n == 0) {
+
+    public static int regressiva(int n) {
+        if (n == 0) {
             return 0;
+        } else {
+            System.out.println(n);
+            return (n - regressiva(n - 1));
+        }
+    }
+
+    public static void fibonacciInt(int n) {
+        int x = 0;
+        int y = 1;
+        int res = 0;
+
+        for (int i = 0; res <= n; i++) {
+            System.out.println(res);
+            res = x;
+            x = y;
+            y = res + y;
+        }
+    }
+
+    public static void fibonacciRec(int n) {
+        if(n == 0){
+            System.out.println(n);
         }
         else{
-            System.out.println(n);
-            return (n - regressiva(n -1));
+            
+        }
+    }
+
+    public static int pow(int n, int m) {
+        if(n >= pow(n, m)){
+            return n;
+        }
+        else{
+            return 1;
         }
     }
 }
